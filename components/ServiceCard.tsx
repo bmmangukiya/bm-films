8e50c3cf0ce446ae79b28b4c25eaf2c2f39a1992
Bansi Mangukiya <bmmangukiya01@gmail.com>
2025-04-18 23:14:11 +0530

    fix: improve layout and styling of ServiceCard component

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -4,14 +4,18 @@ import Link from 'next/link';
 
 function ServiceCard({ title, bg, url }) {
   return (
-    <Link href={url} className="relative cursor-pointer group aspect-square w-[300px] sm:w-[400px] lg:w-[300px] 2xl:w-[400px]">
+    <Link
+      href={url}
+      className="relative cursor-pointer group aspect-square w-[300px] sm:w-[400px] lg:w-[300px] 2xl:w-[400px] overflow-hidden"
+    >
       <Image
         src={bg}
         alt={title}
-        className="shrink max-w-[95dvw] aspect-square object-center transition-all brightness-50 group-hover:brightness-90 group-hover:scale-105 duration-1000 ease-in-out"
+        className="object-cover transition-all duration-1000 ease-in-out group-hover:scale-105 brightness-50 group-hover:brightness-90"
         fill
         loading="lazy"
       />
+
       <div className="absolute inset-0 flex justify-center items-center text-3xl font-extrabold tracking-wider text-white uppercase drop-shadow-lg">
         {title}
       </div>
